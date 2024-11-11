f276b56bc466fc082e957cca13d0067fea7e1d99
k1sihyeon <tlgus530@naver.com>
2024-11-11 21:39:04 +0900

    feat: opencv 기반 TCP 프레임 전송

--- src/v4l2Handler.cpp
+++ src/v4l2Handler.cpp
@@ -1,4 +1,32 @@
 #include "v4l2Handler.hpp"
+#include "tcpHandler.hpp"
+
+#include <opencv2/opencv.hpp>
 
 V4L2Handler::V4L2Handler() {}
 V4L2Handler::~V4L2Handler() {}
+
+void V4L2Handler::StartCapture()
+{
+    cv::VideoCapture cap(0);
+    
+    if (!cap.isOpened()) {
+        std::cerr << "cap is not opened" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    cv::Mat frame;
+    // cap >> frame;
+    // std::cout << frame.type() << std::endl;
+
+    while (true) {
+        cap >> frame;
+        // 전처리
+        
+        // h.264 압축
+
+        // tcp 전송
+        TcpHandler::GetInstance()->SendFrame(frame);
+    }
+
+}
