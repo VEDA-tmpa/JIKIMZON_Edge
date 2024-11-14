78d04c976bac38b48c829990dc05841dd51487e6
k1sihyeon <tlgus530@naver.com>
2024-11-14 21:15:26 +0900

    refactor: newline, namespace const variable

--- test/tutorial/opencv/captureHandler.cpp
+++ test/tutorial/opencv/captureHandler.cpp
@@ -18,19 +18,21 @@ void CaptureHandler::StartCapture()
     // 영상 캡쳐 시작 | 0: 카메라 index | cv::CAP_V4L2: V4L2를 백엔드로 하여 영상을 가져옴
     cv::VideoCapture cap(0, cv::CAP_V4L2);
     
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         std::cerr << "cap is not opened" << std::endl;
         exit(EXIT_FAILURE);
     }
 
     // 영상 캡쳐 Property 설정 | width, height, fps 설정
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, WIDTH);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, HEIGHT);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, SIZE::WIDTH);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, SIZE::HEIGHT);
     cap.set(cv::CAP_PROP_FPS, 30);
 
     cv::Mat frame;
 
-    while (true) {
+    while (true)
+    {
         if (!cap.read(frame))   // 영상으로 부터 프레임 읽기
         {
             // std::cerr << "Failed to capture frame" << std::endl;
