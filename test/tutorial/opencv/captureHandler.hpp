8b847a22a0fcd4b97c6ecc3b8feabad3b9b15a40
k1sihyeon <tlgus530@naver.com>
2024-11-15 14:21:33 +0900

    style: tab

--- test/tutorial/opencv/captureHandler.hpp
+++ test/tutorial/opencv/captureHandler.hpp
@@ -11,14 +11,14 @@ namespace size
 
 class CaptureHandler
 {
-   public:
+public:
     CaptureHandler(const CaptureHandler&) = delete;             // 복사 생성자 삭제
     CaptureHandler& operator=(const CaptureHandler&) = delete;  // 복사연산자 삭제
 
     static CaptureHandler& GetInstance();  // 인스턴스 반환 static 함수
     void StartCapture();                   // 카메라로 부터 캡쳐를 시작하는 함수
 
-   private:
+private:
     CaptureHandler() = default;  // default 생성자, 소멸자를 private으로 선언하여 외부에서의 생성자/소멸자 호출 X
     ~CaptureHandler() = default;
     static CaptureHandler* sInstance;
